76b350c277fd0f608e4dba1d33a5e013f4ca7494
zecawuw <88804350+zecawuw@users.noreply.github.com>
2022-03-12 21:05:37 -0300

    whatsapp-web

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function funcaoButtonPesquisa(){
 
 function funcaoContatos(id){
     document.getElementById("mensagem").style.display = "flex";
+    document.getElementById("pagina-inicial").style.display = "none";
 
     // if(id == "bom-dia"){
     //     document.getElementById("mensagem").style.display = "flex";
@@ -21,3 +22,30 @@ function funcaoContatos(id){
     // }
 }
 
+function funcaoOpcoesMoreVert1(){
+    document.getElementById("opcoes-more-vert").style.display = "inline-block" ;
+    document.getElementById("more-vert-p-1").style.display = "none";
+    document.getElementById("more-vert-p-2").style.display = "inline-block";
+}
+
+function funcaoOpcoesMoreVert2(){
+    document.getElementById("opcoes-more-vert").style.display = "none";
+    document.getElementById("more-vert-p-2").style.display = "none";
+    document.getElementById("more-vert-p-1").style.display = "inline-block";
+}
+
+function fecharConversa(){
+    document.getElementById("pagina-inicial").style.display = "inline-block";
+    document.getElementById("mensagem").style.display = "none";
+}
+
+function dadosContatoCloseAbri(){
+    document.getElementById("dados-do-contato").style.display = "inline-block";
+    document.getElementById("opcoes-more-vert").style.display = "none";
+}
+
+function dadosContatoCloseFechar(){
+    document.getElementById("opcoes-more-vert").style.display = "none";
+    document.getElementById("dados-do-contato").style.display = "none";
+    document.getElementById("mensagem").style.display = "flex";
+}
